90d89ba1dbec2dc48541a316ce43474b559d4892
Chandan <chadan85338818@gmail.com>
2026-03-28 18:54:58 +0530

    fix type any

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -7,12 +7,13 @@ export const authHandler = async (req: Request, res: Response) => {
   const { googleId, email, name, image } = req.body;
 
   try {
+    console.log("🔥 UPSERT START");
     const user = await upsertUser(googleId, email, name, image);
 
     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!, {
       expiresIn: 30 * 24 * 60 * 60,
     });
-
+    console.log("✅ USER CREATED:", user, token);
     return res.status(200).json({ data: user, token });
   } catch (error) {
     const err = error as Error;
