27065fe6501bfbe6825542f3ba7b9bf4f34accf8
Chandan <chadan85338818@gmail.com>
2026-03-28 22:00:00 +0530

    deploy bug proxy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,14 +10,15 @@ import { cloudnaryConfig } from "./config/cloudinary.js";
 import { google } from "googleapis";
 
 const app = express();
-
+app.set("trust proxy", 1);
 // Security Middleware
 app.use(helmet());
 app.use(
   rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
-    message: "Too many requests from this IP, please try again after 15 minutes",
+    message:
+      "Too many requests from this IP, please try again after 15 minutes",
   }),
 );
 
